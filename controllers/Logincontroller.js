2d410a00479f75e211abf6c6ac70303707016253
rehnuma mansuri <rehnumaid567.0@gmail.com>
2026-04-22 15:17:15 +0530

    Correct Rout

--- controllers/Logincontroller.js
+++ controllers/Logincontroller.js
@@ -1,28 +1,26 @@
- const login = async(req,res)=>{
+const User = require("../models/user"); // example import
 
-try{
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-const {email,password} = req.body;
+    const foundUser = await User.findOne({ email });
 
-const user = await user.findOne({email});
+    if (!foundUser) {
+      return res.status(400).json({ message: "User not found" });
+    }
 
-if(!user){
-return res.status(400).json({message:"User not found"});
-}
+    const isMatch = await bcrypt.compare(password, foundUser.password);
 
-const isMatch = await bcrypt.compare(password,user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid password" });
+    }
 
-if(!isMatch){
-return res.status(400).json({message:"Invalid password"});
-}
+    res.json(foundUser);
 
-res.json(user);
-
-}catch(error){
-
-res.status(500).json({error:error.message});
-
-}
-
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
 };
+
 module.exports = login;
