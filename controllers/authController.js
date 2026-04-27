c684614d1392d03cc4784a466d5b33f830678b88
rehnuma mansuri <rehnumaid567.0@gmail.com>
2026-04-27 13:39:00 +0530

    add features

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,62 +1,60 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-// const authService = require('../services/authService');
-const login = require("./Logincontroller");
 
-exports.register = async(req,res)=>{
-  try{
+exports.register = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-    const {name,email,password} = req.body;
-
-    const userExists = await User.findOne({email});
-
-    if(userExists){
-      return res.status(400).json({message:"User already exists"});
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
     }
 
-    const hashedPassword = await bcrypt.hash(password,10);
+    const hashedPassword = await bcrypt.hash(password, 10);
 
     const user = await User.create({
       name,
       email,
-      password:hashedPassword
+      password: hashedPassword
     });
 
     res.status(201).json(user);
 
-  }catch(error){
-    res.status(500).json({error:error.message});
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
 
-exports.login = async(req,res)=>{
-  try{
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
-    const {email,password} = req.body;
+    const user = await User.findOne({ email });
 
-    const user = await User.findOne({email});
-
-    if(!user){
-      return res.status(400).json({message:"Invalid credentials"});
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
     }
 
-    const match = await bcrypt.compare(password,user.password);
+    const match = await bcrypt.compare(password, user.password);
 
-    if(!match){
-      return res.status(400).json({message:"Invalid password"});
+    if (!match) {
+      return res.status(400).json({ message: "Invalid password" });
     }
 
     const token = jwt.sign(
-      {id:user._id},
+      { id: user._id },
       process.env.JWT_SECRET,
-      {expiresIn:"1d"}
+      { expiresIn: "1d" }
     );
 
-    res.json({token,user});
+    res.json({
+      success: true,
+      token,
+      user
+    });
 
-  }catch(error){
-    res.status(500).json({error:error.message});
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
-  module.exports = {login}
 };
